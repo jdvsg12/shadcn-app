cf008e38eb5d73437d8da6dfdbe254713f65c5d7
julian <jdvs_g12@hotmail.com>
2024-11-23 17:20:01 -0500

    add: calendar in single - multiple - range

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -2,18 +2,16 @@
 
 import { Calendar } from "@/components/ui/calendar"
 import { useState } from "react"
+import { DateRange } from "react-day-picker";
 
 
 export default function Page() {
     const [date, setDate] = useState<Date | undefined>(new Date())
+    const [multiple, setMultiple] = useState<Date[] | undefined>()
+    const [range, setRange] = useState<DateRange | undefined>();
 
-    const smallDate = date?.toLocaleDateString("es-ES", {
-        weekday: "short",
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-        timeZone: "UTC",
-    })
+    const fromDate = range?.from
+    const toDate = range?.to
 
     return (
         <section className="grid grid-cols-4 gap-4 place-items-center">
@@ -24,20 +22,36 @@ export default function Page() {
                 className="rounded-md border"
             />
             <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
+                mode="multiple"
+                selected={multiple}
+                onSelect={setMultiple}
+                max={3}
                 className="rounded-md border"
             />
             <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
+                mode="range"
+                selected={range}
+                onSelect={setRange}
                 className="rounded-md border"
             />
-            <div>
+            <div className="flex flex-col gap-2">
                 <h3 className="text-2xl font-bold">Information</h3>
-                {smallDate}
+                <div className="border-2 border-black rounded-lg">
+                    <h2 className="font-bold text-lg">Single</h2>
+                    {date?.toLocaleDateString("es-Es", { weekday: 'short', month: 'short', day: 'numeric' })}
+                </div>
+                <div className="border-2 border-black rounded-lg">
+                    <h2 className="font-bold text-lg">Multiple</h2>
+                    {multiple?.map((date) => (
+                        <p
+                            key={date.toISOString()}>{date.toLocaleDateString("es-Es", { weekday: 'short', month: 'short', day: 'numeric' })}</p>
+                    ))}
+                </div>
+                <div className="border-2 border-black rounded-lg">
+                    <h2 className="font-bold text-lg">Range</h2>
+                    <p><b>From:</b> {fromDate?.toLocaleDateString("es-Es", { weekday: 'short', month: 'short', day: 'numeric' })}</p>
+                    <p><b>To:</b> {toDate?.toLocaleDateString("es-Es", { weekday: 'short', month: 'short', day: 'numeric' })}</p>
+                </div>
             </div>
         </section>
     )
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -35,17 +35,16 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  asChild?: boolean,
-  capitalize?: boolean, // Add an aria-label attribute with the button's text for screen readers. Default is true.
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, capitalize = true, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }), { capitalize: capitalize })}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
